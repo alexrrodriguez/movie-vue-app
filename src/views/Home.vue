650a26b0d10c6e2980ac8764bc0bff868dcef1b1
alex <arodriguez202020@gmail.com>
2021-07-19 19:56:54 -0500

    initial commit, add index and create movie methods and html inputs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h2>Add a Movie!</h2>
+    <button v-on:click="createMovie()">Create Movie</button>
+    <h1>{{ message }}</h1>
+    <div v-for="movie in movies" :key="movie.id">
+      <h2>Title: {{ movie.title }}</h2>
+      <h2>Plot: {{ movie.plot }}</h2>
+    </div>
   </div>
 </template>
-
+<style></style>
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Here are some movies!",
+      movies: [],
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("http://localhost:3000/movies").then((response) => {
+        this.movies = response.data;
+        console.log("Movies:", this.movies);
+      });
+    },
+    createMovie: function () {
+      console.log("added movie!");
+      var params = {
+        title: "Ant Man",
+        year: 2015,
+        plot: "Forced out of his own company by former protégé Darren Cross, Dr. Hank Pym (Michael Douglas) recruits the talents of Scott Lang (Paul Rudd), a master thief just released from prison. Lang becomes Ant-Man, trained by Pym and armed with a suit that allows him to shrink in size, possess superhuman strength and control an army of ants.",
+        director: "Peyton Reed",
+        english: true,
+      };
+      axios
+        .post("http://localhost:3000/movies", params)
+        .then((response) => {
+          console.log("Success!", response.data);
+          this.movies.push(response.data);
+        })
+        .catch((error) => console.log(error.response));
+    },
   },
 };
 </script>
