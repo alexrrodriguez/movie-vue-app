6ea5040f36d3a2d515cb7c9ece1afe8d3a41d28c
alex <arodriguez202020@gmail.com>
2021-07-22 19:41:32 -0500

    create MoviesIndex.vue page to index movies and update index.js routes and path

---
+++ src/views/MoviesIndex.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="movies-index">
+    <h1>All Movies</h1>
+    <div v-for="movie in movies" v-bind:key="movie.id">
+      <h2>{{ movie.title }}</h2>
+      <p>{{ movie.director }}</p>
+      <p>{{ movie.year }}</p>
+      <p>{{ movie.plot }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      movies: [],
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("/movies").then((response) => {
+        console.log("movies index", response);
+        this.movies = response.data;
+      });
+    },
+  },
+};
+</script>
